4ccf7b812b60466eb5581b5af0886c2f0436593e
Cheref wassim <95647708+xwassim@users.noreply.github.com>
2023-04-03 01:45:22 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import topbar from './Components/topbar.jsx';
+import FORM from './Components/FORM.jsx';
+
+function App() {
+  return (
+    <div>
+      <FORM/>
+    </div>
+  );
+}
+
+export default FORM;
---
+++ src/Components/topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
+import {BsChatSquareDots} from 'react-icons/bs'
+
+const topbar = () => {
+  return (
+    <div>
+        <div>
+            <BsChatSquareDots/>
+            <h1>WEEKWAY</h1>
+        </div>
+    </div>
+  )
+}
+
+export default topbar
